e86347b6da328eaefd17943d61bf77349a2b64a3
willmartolentino <willmar.tolentino@ninthdesign.com>
2013-11-21 16:15:33 +0800

    Revise markup (index) and Try test functions and loops (js)

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,33 @@
-var item = document.getElementsByClassName("backslidr-item item");
+var slidrBtns = document.getElementsByClassName('backslidr-btn'); // Selects all HTML elements with .backslidr-btn class
 
-// console.log(item);
+/*==========  This function will execute when the user click/s on any image thumbnail: Start  ==========*/
+var slidrFade = function (event) {
+    var targetElement = this.hash; // Targets the element anchored on the .backslidr-btn class
+    console.log(targetElement);
 
-var slidrBtn1 = function () {
-	item[4].style.display = "block";
-	item[0].style.display = "block";
-}
-
-var slidrBtn2 = function () {
-	item[0].style.display = "none";
-	item[1].style.display = "block";
-}
-
-var slidrBtn3 = function () {
-	item[1].style.display = "none";
-	item[2].style.display = "block";
-}
-
-var slidrBtn4 = function () {
-	item[2].style.display = "none";
-	item[3].style.display = "block";
-}
-
-var slidrBtn5 = function () {
-	item[3].style.display = "none";
-	item[4].style.display = "block";
-}
-
-
-
-// var slidrFadeIn.onclick = function () {
-
-// }
-
-// var slidrFadeOut.onclick = function () {
+    var targetVal = targetElement.href = targetElement; // example value: #backslidr-item-4
     
-// }
+    console.log(targetVal);
+    // var delHash = str.slice(1); // expected result: backslidr-item-4
+
+    // targetElement.delHash;
+
+    //document.getElementById(targetElement).
+
+    // var el = document.getElementById("backslidr-items").childNodes;
+    // for (var j = 0; j < el.length; j++) {
+    // console.log(el[j].nodeName + " <br />");
+    // };
+    
+
+    event.preventDefault();
+};
+
+for (var i = 0; i < slidrBtns.length; i++) { // Loop
+    slidrBtns[i].addEventListener('click', slidrFade, false); // Add the event listener to all selected HTML elements with .backslidr-btn class
+}
+
+/*==========  This function will execute when the user click/s on any image thumbnail: End  ==========*/
+
+// var slidrFadeIn.onclick = function () {}
+// var slidrFadeOut.onclick = function () {}
